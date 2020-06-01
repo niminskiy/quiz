4190dcfcfaa2a9a862e27d55ccab0f9b653f1df9
Sergey Niminskiy <s.niminskiy@gmail.com>
2020-06-01 10:18:38 +0300

    Загрузил проект

---
+++ script/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const btnOpenModal = document.querySelector('#btnOpenModal');
+    const modalBlock = document.querySelector('#modalBlock');
+    const closeModal = document.querySelector('#closeModal');
+
+    btnOpenModal.addEventListener('click', () => {
+        modalBlock.classList.add('d-block');
+        playTest();
+    })
+
+    closeModal.addEventListener('click', () => {
+        modalBlock.classList.remove('d-block');
+    })
+
+    const playTest = () => {
+        const renderQuestions = () => {
+            console.log('render')
+        }        
+    }
+})
