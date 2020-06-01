793bf2dde8c8bc6f406382e56d765995fa5b6448
niminskiy <s.niminskiy@gmail.com>
2020-06-01 14:03:17 +0300

    Удалил строку

--- script/script.js
+++ script/script.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block');
         playTest();
-        console.log()
     })
 
     closeModal.addEventListener('click', () => {
